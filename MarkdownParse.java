ef0825ee8397c1ba9ef9df8f76a093d361cba51c
Erwin Madjus <erwinmadjusbusiness@gmail.com>
2022-02-27 00:46:55 -0800

    Edits v1

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -5,47 +5,33 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParse {
-
-    // Loop with a stack until finding the corresponding closeParen
-    static int findCloseParen(String markdown, int openParen) {
-        int closeParen = openParen + 1;
-        int openParenCount = 1;
-        while (openParenCount > 0) {
-            if (markdown.charAt(closeParen) == '(') {
-                openParenCount++;
-            } else if (markdown.charAt(closeParen) == ')') {
-                openParenCount--;
-            }
-            closeParen++;
-        }
-        return closeParen - 1;
-
-    }
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            // System.out.format("%d\t%d\t%s\n", currentIndex, nextOpenBracket, toReturn);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
+            int nextOpenBracket = markdown.indexOf("[", currentIndex),
+                nextCloseBracket = markdown.indexOf("]", nextOpenBracket),
+                openParen = markdown.indexOf("(", nextCloseBracket),
+                closeParen = markdown.indexOf(")", openParen);
 
-            // The close paren we need may not be the next one in the file
-            int closeParen = findCloseParen(markdown, openParen);
-            
-            if(nextOpenBracket == -1 || nextCloseBracket == -1
-                  || closeParen == -1 || openParen == -1) {
-                return toReturn;
-            }
-            String potentialLink = markdown.substring(openParen + 1, closeParen).trim();
-            if(potentialLink.indexOf(" ") == -1 && potentialLink.indexOf("\n") == -1) {
-                toReturn.add(potentialLink);
+            if(nextOpenBracket > 0 &&
+               markdown.charAt(nextOpenBracket-1) == '!'){
+                // Image, skip
+                currentIndex = nextCloseBracket + 1;
+            } else if(nextOpenBracket >= 0 &&
+                      nextCloseBracket >= 0 &&
+                      nextCloseBracket > nextOpenBracket+1 &&
+                      openParen == nextCloseBracket+1 &&
+                      closeParen >= 0
+            ){
+                // Valid link, add to list
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
                 currentIndex = closeParen + 1;
-            }
-            else {
-                currentIndex = currentIndex + 1;
+            } else {
+                // Invalid link, advance one character
+                currentIndex += 1;
             }
         }
         return toReturn;
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,5 +1,36 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void getLinks() throws IOException {
+        String Test = Files.readString(Path.of("TestFile.md"));
+        assertEquals(List.of("https://something.com", "some-page.html"),
+        MarkdownParse.getLinks(Test));
+    }
+
+    @Test
+    public void getLinks2() throws IOException {
+        String Test2 = Files.readString(Path.of("TestFile.md"));
+        assertEquals(List.of("https://something.com", "some-page.html"),
+        MarkdownParse.getLinks(Test2));
+    }
+}
+
+
+
+
+/*import static org.junit.Assert.*;
+import org.junit.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,8 +44,14 @@ public class MarkdownParseTest {
         List<String> expect = List.of("https://something.com", "some-page.html");
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
-    
-    @Test
+}    
+*/
+
+
+
+
+
+/*    @Test
     public void testFile2() throws IOException {
         String contents= Files.readString(Path.of("./test-file2.md"));
         List<String> expect = List.of("https://something.com", "some-page.html");
@@ -36,3 +73,5 @@ public class MarkdownParseTest {
     }
 
 }
+
+*/
